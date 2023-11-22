dbabbcfaf3f6f88e6616968011c125a1ca69d01e
Netervati <netervati@gmail.com>
2023-11-22 17:02:15 +0800

    Support deleting of model data

--- functions/index.ts
+++ functions/index.ts
@@ -4,6 +4,8 @@ import healthCheck from './healthCheck';
 
 export default function (app: FastifyInstance, _opts: FastifyServerOptions, done: () => void) {
   app.get('/', healthCheck.get);
+
+  app.delete('/:model/:id', models.delete);
   app.get('/:model/:id', models.find);
   app.get('/:model', models.list);
   app.post('/:model', models.create);
--- functions/models.ts
+++ functions/models.ts
@@ -108,6 +108,51 @@ const routes: ServerlessRoutes = {
 
     reply.send(formatResponse(modelData.data[0]));
   },
+  delete: async function (request, reply) {
+    const { id, model } = request.params as Params;
+    const { data, error } = await getApp(request);
+
+    if (error) {
+      return reply.status(error.status).send({
+        error: error.message,
+      });
+    }
+
+    const record = await supabase.from('models')
+      .select('id, model_data!inner(id, schema)')
+      .is('deleted_at', null)
+      .is('model_data.deleted_at', null)
+      .eq('model_data.schema->>id', id)
+      .eq('name', model)
+      .eq('app_id', data.id);
+
+    if (record.error) {
+      return reply.status(record.status).send({
+        error: record.error.message,
+      });
+    }
+
+    if (record.data.length == 0) {
+      return reply.status(404).send({
+        error: `${model} with id ${id} not found.`,
+      });
+    }
+
+    const modelData = await supabase.from('model_data')
+      .update({
+        deleted_at: new Date().toISOString(),
+      })
+      .eq('id', record.data[0].model_data[0].id)
+      .select('*');
+
+    if (modelData.error) {
+      return reply.status(modelData.status).send({
+        error: modelData.error.message,
+      });
+    }
+
+    return reply.status(204).send();
+  },
   find: async function (request, reply) {
     const { id, model } = request.params as Params;
     const { data, error } = await getApp(request);
